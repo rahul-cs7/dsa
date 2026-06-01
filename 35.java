7411dd5a7980acda4abbb06de0814b83b8fd439b
rahul-cs7 <rahuluu2327@gmail.com>
2026-06-01 12:23:28 +0530

    question 35 array

---
+++ 35.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter sorted array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        Solution obj = new Solution();
+
+        int result = obj.searchInsert(nums, target);
+
+        System.out.println("Insert Position = " + result);
+
+        sc.close();
+    }
+}
